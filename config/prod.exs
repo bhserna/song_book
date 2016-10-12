9eb14b85272907f5f4926f0270844f0cd2b4a163
bhserna <bhserna@gmail.com>
2016-10-11 22:13:21 -0500

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :song_book_web, SongBookWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "song-book.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,6 +59,12 @@ config :logger, level: :info
 #     config :song_book_web, SongBookWeb.Endpoint, server: true
 #
 
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
